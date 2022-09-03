ba4d3a3fd07f69b0216fb56bafa46584c630eaa9
Minh Van Nguyen <van23112002@gmail.com>
2022-09-04 02:51:23 +0700

    Updates

---
+++ precache-manifest.0d4deab7ff3b0774afcbbc01ce85cb0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01ae519f0bb8e3880452",
+    "url": "/schedule-manager/static/css/main.e8f3c4b3.chunk.css"
+  },
+  {
+    "revision": "01ae519f0bb8e3880452",
+    "url": "/schedule-manager/static/js/main.01ae519f.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/schedule-manager/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "ce5331eb94700db4843f",
+    "url": "/schedule-manager/static/js/2.ce5331eb.chunk.js"
+  },
+  {
+    "revision": "3bf5d8171971baec7b44",
+    "url": "/schedule-manager/static/js/runtime~main.3bf5d817.js"
+  },
+  {
+    "revision": "7786ee6b8cee6ca18c2b6e447fb4b731",
+    "url": "/schedule-manager/index.html"
+  }
+];
